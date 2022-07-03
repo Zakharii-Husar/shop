75b44fe9b470e42a405eb68b2e5630c38608ac9c
Zakharii-Husar <zakhariihusar@gmail.com>
2022-07-03 19:00:48 +0300

    switched from browser router to hash router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Item from './components/Item';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route,
 } from "react-router-dom";
@@ -18,7 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" exact element={<App />}>
             <Route path="/cart" element={<Cart />} />
@@ -27,7 +27,7 @@ root.render(
             <Route path="/:section/:item" element={<Item />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
