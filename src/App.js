78e100bc2d8e3efebd666498cd3a80d787bb24fd
Zakharii-Husar <zakhariihusar@gmail.com>
2022-07-10 13:25:58 +0300

    test git

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './style/App.scss'
 import Navbar from './components/Navbar';
 import Fetch from './features/fetch/Fetch';
 
-
+//tesdt git
 function App() {
   return (
     <div className="App">
